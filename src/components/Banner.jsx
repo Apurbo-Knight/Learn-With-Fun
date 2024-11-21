6079fb404de9e4952c5b98faf0fe35d69c6b5c0f
Apurbo Das <akdjoy29@gmail.com>
2024-11-21 11:43:58 +0600

    rounded banner image

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -7,8 +7,8 @@ const Banner = () => {
   return (
     
       <div className="carousel w-full mb-10">
-        <div id="slide1" className="carousel-item relative w-full">
-          <img src={img1} className="w-full h-[600px]" />
+        <div id="slide1" className="carousel-item relative w-full ">
+          <img src={img1} className="w-full h-[600px] rounded-md" />
           <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
             <a href="#slide4" className="btn btn-circle">
               ❮
@@ -19,7 +19,7 @@ const Banner = () => {
           </div>
         </div>
         <div id="slide2" className="carousel-item relative w-full">
-          <img src={img2} className="w-full h-[600px]" />
+          <img src={img2} className="w-full h-[600px] rounded-md" />
           <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
             <a href="#slide1" className="btn btn-circle">
               ❮
@@ -30,7 +30,7 @@ const Banner = () => {
           </div>
         </div>
         <div id="slide3" className="carousel-item relative w-full">
-          <img src={img3} className="w-full h-[600px]" />
+          <img src={img3} className="w-full h-[600px] rounded-md" />
           <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
             <a href="#slide2" className="btn btn-circle">
               ❮
@@ -41,7 +41,7 @@ const Banner = () => {
           </div>
         </div>
         <div id="slide4" className="carousel-item relative w-full">
-          <img src={img4} className="w-full h-[600px]" />
+          <img src={img4} className="w-full h-[600px] rounded-md" />
           <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
             <a href="#slide3" className="btn btn-circle">
               ❮
